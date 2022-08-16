27a4a98a5fe8ae5d2529fa5be15cc8ee9e5165c6
1nnovator <1nnovator1105@gmail.com>
2022-08-16 22:03:04 +0900

    22.08.16 javascript

---
+++ markdown/자바스크립트_싱글스레드/javascript.js
@@ -0,0 +1,32 @@
+// function print2() {
+//     console.log('2')
+// }
+
+// function doSomething() {
+//     console.log('doSomething');
+// }
+
+// function main() {
+//     console.log('1');
+//     print2();
+//     doSomething();
+// }
+
+// main();
+
+// console.log('start');
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 1000)
+// console.log('end');
+
+// document.getElementById('btn').addEventListener('click', () => {
+//     alert('hi');
+//     console.log('bye')
+// })
+
+console.log('start');
+setTimeout(() => {
+    console.log('timeout');
+}, 0)
+console.log('end');
